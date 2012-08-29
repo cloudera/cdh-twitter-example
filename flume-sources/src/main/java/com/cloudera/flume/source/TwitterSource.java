2b930878293825317e83405376639e549d7d84ab
Jon Natkins <natty@cloudera.com>
2012-08-29 16:18:27 -0700

    Custom Flume source and Flume configuration
    This adds a Flume source which will access the Twitter Streaming API and stream
    raw JSON tweets through Flume. The configuration defines a setup which will
    take the JSON data and load it into HDFS via a Memory Channel.

---
+++ flume-sources/src/main/java/com/cloudera/flume/source/TwitterSource.java
@@ -0,0 +1,150 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cloudera.flume.source;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.EventDrivenSource;
+import org.apache.flume.channel.ChannelProcessor;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.event.EventBuilder;
+import org.apache.flume.source.AbstractSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.json.DataObjectFactory;
+
+/**
+ * A Flume Source, which pulls data from Twitter's streaming API. Currently,
+ * this only supports pulling from the sample API, and only gets new status
+ * updates.
+ */
+public class TwitterSource extends AbstractSource
+    implements EventDrivenSource, Configurable {
+  
+  private static final Logger logger =
+      LoggerFactory.getLogger(TwitterSource.class);
+
+  /** Information necessary for accessing the Twitter API */
+  private String consumerKey;
+  private String consumerSecret;
+  private String accessToken;
+  private String accessTokenSecret;
+  
+  /** Size of event batches */
+  private long batchSize;
+  
+  /** The actual Twitter stream. It's set up to collect raw JSON data */
+  private final TwitterStream twitterStream = new TwitterStreamFactory(
+      new ConfigurationBuilder()
+        .setJSONStoreEnabled(true)
+        .build()).getInstance();
+
+  /**
+   * The initialization method for the Source. The context contains all the
+   * Flume configuration info, and can be used to retrieve any configuration
+   * values necessary to set up the Source.
+   */
+  @Override
+  public void configure(Context context) {
+    consumerKey = context.getString(TwitterSourceConstants.CONSUMER_KEY_KEY);
+    consumerSecret = context.getString(TwitterSourceConstants.CONSUMER_SECRET_KEY);
+    accessToken = context.getString(TwitterSourceConstants.ACCESS_TOKEN_KEY);
+    accessTokenSecret = context.getString(TwitterSourceConstants.ACCESS_TOKEN_SECRET_KEY);
+    
+    batchSize = context.getLong(TwitterSourceConstants.BATCH_SIZE_KEY,
+        TwitterSourceConstants.DEFAULT_BATCH_SIZE);
+  }
+
+  /**
+   * Start processing events. This uses the Twitter Streaming API to sample
+   * Twitter, and process tweets.
+   */
+  @Override
+  public void start() {
+    // The channel is the piece of Flume that sits between the Source and Sink,
+    // and is used to process events.
+    final ChannelProcessor channel = getChannelProcessor();
+    
+    final List<Event> eventList = new ArrayList<Event>();
+    final Map<String, String> headers = new HashMap<String, String>();
+    
+    // The StatusListener is a twitter4j API, which can be added to a Twitter
+    // stream, and will execute methods every time a message comes in through
+    // the stream.
+    StatusListener listener = new StatusListener() {
+      // The onStatus method is executed every time a new tweet comes in.
+      public void onStatus(Status status) {
+        // The EventBuilder is used to build an event using the headers and
+        // the raw JSON of a tweet
+        eventList.add(EventBuilder.withBody(
+            DataObjectFactory.getRawJSON(status).getBytes(), headers));
+        
+        // When we've filled up a batch, we use the channel to process the
+        // list of events.
+        if (eventList.size() >= batchSize) {
+          logger.debug("Processing a batch of {} events", eventList.size());
+          channel.processEventBatch(eventList);
+          eventList.clear();
+        }
+      }
+      
+      // This listener will ignore everything except for new tweets
+      public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
+      public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
+      public void onScrubGeo(long userId, long upToStatusId) {}
+      public void onException(Exception ex) {}
+    };
+    
+    logger.debug("Setting up Twitter sample stream using consumer key {} and" +
+          " access token {}", new String[] { consumerKey, accessToken });
+    // Set up the stream's listener (defined above), and set any necessary
+    // security information.
+    twitterStream.addListener(listener);
+    twitterStream.setOAuthConsumer(consumerKey, consumerSecret);
+    AccessToken token = new AccessToken(accessToken, accessTokenSecret);
+    twitterStream.setOAuthAccessToken(token);
+    
+    // Start sampling Twitter!
+    twitterStream.sample();
+    super.start();
+  }
+  
+  /**
+   * Stops the Source's event processing and shuts down the Twitter stream.
+   */
+  @Override
+  public void stop() {
+    logger.debug("Shutting down Twitter sample stream...");
+    twitterStream.shutdown();
+    super.stop();
+  }
+}
---
+++ flume-sources/src/main/java/com/cloudera/flume/source/TwitterSourceConstants.java
@@ -0,0 +1,13 @@
+package com.cloudera.flume.source;
+
+public class TwitterSourceConstants {
+
+  public static final String CONSUMER_KEY_KEY = "consumerKey";
+  public static final String CONSUMER_SECRET_KEY = "consumerSecret";
+  public static final String ACCESS_TOKEN_KEY = "accessToken";
+  public static final String ACCESS_TOKEN_SECRET_KEY = "accessTokenSecret";
+  
+  public static final String BATCH_SIZE_KEY = "batchSize";
+  public static final long DEFAULT_BATCH_SIZE = 1000L;
+
+}
